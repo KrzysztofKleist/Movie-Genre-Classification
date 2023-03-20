c30c151e61db7f41f8fef692da91c3421a72049c
Krzysztof Kleist s296450 <s296450@studenti.polito.it>
2023-03-20 17:35:52 +0100

    minor changes

--- utilities.py
+++ utilities.py
@@ -33,67 +33,71 @@ def video_available(link):
 
 def gray_scale_check(link):
     url = 'https://www.youtube.com/watch?v=' + link
-
-    vPafy = pafy.new(url)
-    play = vPafy.getbestvideo(preftype='webm')
-
-    video = cv2.VideoCapture(play.url)
-
-    fps = int(video.get(cv2.CAP_PROP_FPS))
-
-    # vid_len = vPafy.length
-
-    # we want to extract one frame from the 2 second of the video
-    frame_num = int(fps * 2)
-
-    # print('frame to extract:', frame_num)
-
-    current_frame = 0
-
-    while(True):
-        # print(current_frame)
-        # read frame
-        ret, frame = video.read()
-
-        if ret:
-            if current_frame == frame_num:
-                cv2.imshow('frame', frame)
-
-                # splitting b, g, r channels
-                b, g, r = cv2.split(frame)
-
-                # getting differences between (b,g), (r,g), (b,r) channel pixels
-                r_g = np.count_nonzero(abs(r-g))
-                r_b = np.count_nonzero(abs(r-b))
-                g_b = np.count_nonzero(abs(g-b))
-
-                # sum of differences
-                diff_sum = float(r_g+r_b+g_b)
-
-                # finding ratio of diff_sum with respect to size of image
-                ratio = diff_sum/frame.size
-
-                if ratio > 0.005:
-                    # print("image is color")
-                    ret_val = True
-                else:
-                    # print("image is greyscale")
-                    ret_val = False
-
-                break
-                # current_frame += 1
-            else:
-                current_frame += 1
-        else:
-            ret_val = False
-            break
-
-        # if current_frame > frame_num:
-        #     break
-
-    # release VideoCapture
-    video.release()
-
-    cv2.destroyAllWindows()
     
-    return ret_val
+    try:
+        vPafy = pafy.new(url)
+        play = vPafy.getbestvideo(preftype='webm')
+
+        video = cv2.VideoCapture(play.url)
+
+        fps = int(video.get(cv2.CAP_PROP_FPS))
+
+        # vid_len = vPafy.length
+
+        # we want to extract one frame from the 2 second of the video
+        frame_num = int(fps * 2)
+
+        # print('frame to extract:', frame_num)
+
+        current_frame = 0
+
+        while(True):
+            # print(current_frame)
+            # read frame
+            ret, frame = video.read()
+
+            if ret:
+                if current_frame == frame_num:
+                    # cv2.imshow('frame', frame)
+
+                    # splitting b, g, r channels
+                    b, g, r = cv2.split(frame)
+
+                    # getting differences between (b,g), (r,g), (b,r) channel pixels
+                    r_g = np.count_nonzero(abs(r-g))
+                    r_b = np.count_nonzero(abs(r-b))
+                    g_b = np.count_nonzero(abs(g-b))
+
+                    # sum of differences
+                    diff_sum = float(r_g+r_b+g_b)
+
+                    # finding ratio of diff_sum with respect to size of image
+                    ratio = diff_sum/frame.size
+
+                    if ratio > 0.005:
+                        # print("image is color")
+                        ret_val = True
+                    else:
+                        # print("image is greyscale")
+                        ret_val = False
+
+                    break
+                    # current_frame += 1
+                else:
+                    current_frame += 1
+            else:
+                ret_val = False
+                break
+
+            # if current_frame > frame_num:
+            #     break
+
+        # release VideoCapture
+        video.release()
+
+        cv2.destroyAllWindows()
+        
+        return ret_val
+    
+    except:
+        return False
