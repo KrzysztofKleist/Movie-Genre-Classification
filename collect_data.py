e29901e1cac64b26204f7780acc2126af088388f
Krzysztof Kleist s296450 <s296450@studenti.polito.it>
2023-03-30 15:46:48 +0200

    implementing multiprocessing

---
+++ collect_data.py
@@ -0,0 +1,50 @@
+from utilities import extract_frame, color_check, video_available, check_folder
+from multiprocessing import Pool
+import pandas as pd
+import time
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+def multi_run_wrapper(args):
+    return download_data(*args)
+
+
+final_df = pd.read_csv(r'files\movieclips_final.tsv', sep="\t")
+# final_df = final_df.sample(frac=1).reset_index(drop=True)
+
+
+def download_data(start_range, end_range):
+    
+    print("####################################################################################")
+    print("Current step:", start_range)
+    print("####################################################################################")
+    
+    # loop through each row in the dataframe
+    for index, row in final_df.iloc[start_range:end_range].iterrows():
+        if check_folder(row['videoid'], row['genre'], r'D:\frames') == True:
+            if video_available(row['videoid']) == True and color_check(row['videoid']) == True:
+                extract_frame(row['videoid'], row['genre'],
+                              2, 20, r'D:\frames')
+
+
+if __name__ == '__main__':
+
+    start = time.time()
+
+    n = 2000
+    step = 50
+    intervals = [(i, i+step) for i in range(0, n, step) if i+step <= n]
+    last_interval = [(n-(n % step), n)]
+    intervals = intervals + last_interval
+    print(intervals)
+
+    pool = Pool(10)
+
+    pool.map(multi_run_wrapper, intervals)
+
+    pool.close()
+    pool.join()
+
+    end = time.time()
+    print('time:', end - start)
--- utilities.py
+++ utilities.py
@@ -18,6 +18,21 @@ def list_to_string(genre):
     return '_'.join(genre.replace("[", "").replace("]", "").replace("'", "").replace(" ", "").split(","))
 
 
+def check_folder(link, genre, folder_path):
+
+    genre_str = genre.replace("'", "").replace(
+        "[", "").replace("]", "").replace(" ", "").replace(",", "_")
+
+    if os.path.exists(folder_path + "/" + genre_str + "/" + link):
+        # if folder exists return False
+        print(folder_path + "/" + genre_str + "/" + link + " exists")
+        return False
+    else:
+        # if folder DOESN'T exist return True
+        print(folder_path + "/" + genre_str + "/" + link + " DOESN'T exists")
+        return True
+
+
 def video_available(link):
     # checking if video is available
     url = 'https://www.youtube.com/watch?v=' + link
@@ -31,9 +46,9 @@ def video_available(link):
         return False
 
 
-def gray_scale_check(link):
+def color_check(link):
     url = 'https://www.youtube.com/watch?v=' + link
-    
+
     try:
         vPafy = pafy.new(url)
         play = vPafy.getbestvideo(preftype='webm')
@@ -51,7 +66,7 @@ def gray_scale_check(link):
 
         current_frame = 0
 
-        while(True):
+        while (True):
             # print(current_frame)
             # read frame
             ret, frame = video.read()
@@ -96,8 +111,77 @@ def gray_scale_check(link):
         video.release()
 
         cv2.destroyAllWindows()
-        
+
         return ret_val
-    
+
     except:
         return False
+
+
+def extract_frame(link, genre, start_frame, interval, folder_path):
+
+    genre_str = genre.replace("'", "").replace(
+        "[", "").replace("]", "").replace(" ", "").replace(",", "_")
+
+    if os.path.exists(folder_path + "/" + genre_str + "/" + link):
+        print(folder_path + "/" + genre_str + "/" + link + " exists")
+        return
+    else:
+        create_dir(folder_path + "/" + genre_str + "/" + link)
+        print("Creating folder..." + folder_path + "/" + genre_str)
+
+        url = 'https://www.youtube.com/watch?v=' + link
+
+        vPafy = pafy.new(url)
+        play = vPafy.getbestvideo(preftype='webm')
+
+        video = cv2.VideoCapture(play.url)
+
+        fps = int(video.get(cv2.CAP_PROP_FPS))
+
+        vid_len = vPafy.length
+
+        # we want to get the first frame at start_frame seconds
+        # and then each frame after the next interval seconds
+        start_value = start_frame * fps
+        interval_fps = interval * fps
+        # we stop at the 3/4 of the video because of the fact
+        # that videos usually contain some ending credits
+        stop_value = int(3/4 * fps * vid_len)
+
+        frame_numbers = [start_value]
+
+        while frame_numbers[-1] + interval_fps < stop_value:
+            frame_numbers.append(frame_numbers[-1] + interval_fps)
+
+        current_frame = 0
+
+        while (True):
+            # read frame
+            ret, frame = video.read()
+
+            if ret:
+                if current_frame in frame_numbers:
+                    name = folder_path + "/" + genre_str +\
+                        "/" + link + "/" + str(current_frame) + '.jpg'
+
+                    if os.path.isfile(name):
+                        print(name + "exists")
+                    else:
+                        print("Creating..." + name)
+                        cv2.imwrite(name, frame)
+
+                    # shutil.move(name, dest)
+                    current_frame += 1
+                else:
+                    current_frame += 1
+            else:
+                break
+
+            if current_frame > frame_numbers[-1]:
+                break
+
+        # release VideoCapture
+        video.release()
+
+        cv2.destroyAllWindows()
